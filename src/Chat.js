419c19a60fba997434e859bbc15dff9aeca11c08
alii13 <shekhaliul44@gmail.com>
2020-09-05 06:24:34 +0530

    [Fix] Sound Added

--- src/Chat.js
+++ src/Chat.js
@@ -13,6 +13,8 @@ import { useParams } from 'react-router-dom';
 import { useStateValue } from './StateProvider'
 import { actionTypes } from "./reducer";
 import UseWindowDimensions from "./UseWindowDimensions";
+import useSound from 'use-sound';
+// import recievesoundURL from "./recieve.mp3"
 // import DrawerPhone from "./DrawerPhone";
 function Chat() {
 
@@ -24,9 +26,29 @@ function Chat() {
     const [toggler, setToggler]=useState(true)
     const displayName =localStorage.getItem('displayName');
     const [{togglerState}, dispatch]= useStateValue();
+    const [issendChecked, setIssendChecked]=useState(false);
+    // const [isRecChecked, setIsRecChecked]=useState(1);
      const { width } = UseWindowDimensions();
 
     //console.log(roomId);
+
+    const [playOn] = useSound(
+        `${process.env.PUBLIC_URL}/send.mp3`,
+        { volume: 0.5 }
+      );
+      const [playOff] = useSound(
+        `${process.env.PUBLIC_URL}/send.mp3`,
+        { volume: 0.5 }
+      );
+
+    //   const [playOnRec] = useSound(
+    //     sendsoundURL,
+    //     { volume: 0.5 }
+    //   );
+    //   const [playOffRec] = useSound(
+    //     sendsoundURL,
+    //     { volume: 0.5 }
+    //   );
     useEffect(()=>{
       setSeed(Math.floor(Math.random() * 5000));
         if(roomId){
@@ -37,11 +59,13 @@ function Chat() {
             db.collection('rooms').doc(roomId).collection("messages").orderBy('timestamp','asc').onSnapshot(snapshot => {
                 setMessages(snapshot.docs.map(doc =>doc.data()));
             })
+           
         }
 
     },[roomId])
 
  // console.log(togglerState);
+ 
     
     const sendMessage=(e)=>{
         e.preventDefault();
@@ -51,6 +75,8 @@ function Chat() {
             name:displayName,
             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         })
+        setIssendChecked(!issendChecked);
+        issendChecked ? playOff() : playOn();
         setInput("");
     };
     const messagesEndRef = useRef(null);
@@ -172,7 +198,7 @@ function Chat() {
                     <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
                     <div className="chat__headerInfo">
                         <h3>{roomName}</h3>
-                        <p className="header__lastSeen">last seen {messages.length!==0?(messages[messages.length-1].timestamp?.toDate().toUTCString()):"Loading"}</p>
+                        <p className="header__lastSeen">last seen {messages.length!==0?(String(messages[messages.length-1].timestamp?.toDate().toUTCString()).slice(0,22) ):"Loading"}</p>
                     </div>
                     <div className="chat__headerRight">
                         <IconButton>
@@ -189,9 +215,9 @@ function Chat() {
                 <div className="chat__body">
                     {messages.map((message) => (
                         <p className={`chat__messages ${(message.name===displayName) && 'chat__reciever'}`}>
-                        <span className="chat__name">{message.name}</span>{message.message}
+                        <span className="chat__name">{message.name }</span>{message.message}
                         <span className="chat__timestamp">
-                         {new Date(message.timestamp?.toDate()).toUTCString()}  </span></p>
+                         {String(new Date(message.timestamp?.toDate()).toUTCString()).slice(5,12) + String(new Date(message.timestamp?.toDate()).toUTCString()).slice(17,22)}  </span></p>
                     ))}
                     <div ref={messagesEndRef} />
                 </div>

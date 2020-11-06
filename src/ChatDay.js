9c387c3fe1654fd1e61bc7e8072cec619fea7d38
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-11-06 06:39:11 +0530

    Code Cleaned & Formatted

--- src/ChatDay.js
+++ src/ChatDay.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function ChatDay(props) {
-
-    return (
-     <div className="chat__body__daystamp">
-        <p className="chat__body__daystamp__title">{props.date}</p>
-     </div>
-    )
+  return (
+    <div className="chat__body__daystamp">
+      <p className="chat__body__daystamp__title">{props.date}</p>
+    </div>
+  );
 }
 
-export default ChatDay
+export default ChatDay;
--- src/UseWindowDimensions.js
+++ src/UseWindowDimensions.js
@@ -1,24 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
-    height
+    height,
   };
 }
 
 export default function UseWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-  
-    useEffect(() => {
-      function handleResize() {
-        setWindowDimensions(getWindowDimensions());
-      }
-  
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
-    }, []);
-  
-    return windowDimensions;
-  }
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
